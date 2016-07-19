2eae3385fef340a3b6beb9dc137da3ee45dd18b5
Adrian Martin <adrianmartin@Adrians-MacBook-Pro.local>
2016-07-19 16:06:28 -0700

    added script to js in index and added .toUpperCase

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ alert('Hi, ' + userName + ', welcome! Let\'s see if you can answer some question
 
 var answer1 = prompt('Is Adrian from Montreal, QC?');
 console.log(answer1);
-if (answer1 === 'no' || answer1 === 'n') {
+if (answer1 === 'no'.toUpperCase() || answer1 === 'n'.toUpperCase()) {
   alert('You got it right, ' + userName + '!');
 } else {
   alert('Ew, ' + userName + ', you got it wrong. I\'m from Vancouver, BC.');
@@ -15,7 +15,7 @@ if (answer1 === 'no' || answer1 === 'n') {
 
 var answer2 = prompt('Does Adrian enjoy skateboarding?');
 console.log(answer2);
-if (answer2 === 'yes' || answer2 === 'y') {
+if (answer2 === 'yes'.toUpperCase() || answer2 === 'y'.toUpperCase()) {
   alert('I sure do, ' + userName + '!');
 } else {
   alert('Actually, I do! I used to skate in highschool all the time.');
@@ -23,15 +23,15 @@ if (answer2 === 'yes' || answer2 === 'y') {
 
 var answer3 = prompt('Did Adrian eat a bagel for lunch on July 19, 2016?');
 console.log(answer3);
-if (answer3 === 'yes' || answer3 === 'y') {
-  alert('Yep! You\'re correct, ' + userName + '!');
+if (answer3 === 'yes'.toUpperCase() || answer3 === 'y'.toUpperCase()) {
+  alert('Yep! You\'re correct, ' + userName + '! It was delicious!');
 } else {
   alert('Nope. You\'re wrong, ' + userName + '. I actually went to some place at the Seattle Center.');
 }
 
 var answer4 = prompt('Did Adrian play pee-wee hockey when he was a kid?');
 console.log(answer4);
-if (answer4 === 'yes' || answer4 === 'y') {
+if (answer4 === 'yes'.toUpperCase() || answer4 === 'y'.toUpperCase()) {
   alert('Yes, that\'s right. Would have stuck with it if I didn\'t suck so bad.');
 } else {
   alert('No, I did. Something I honestly regret giving up so early on! Maybe I could have been the shortest player in the NHL.');
@@ -39,7 +39,7 @@ if (answer4 === 'yes' || answer4 === 'y') {
 
 answer5 = prompt('Did Adrian play guitar for a band during high school?');
 console.log(answer5);
-if (answer5 === 'no' || answer5 === 'n') {
+if (answer5 === 'no'.toUpperCase() || answer5 === 'n'.toUpperCase()) {
   alert('You\'re right, ' + userName + '! I actually played drums in high school');
 } else {
   alert('Nope! I actually played drums for a band in high school.');
